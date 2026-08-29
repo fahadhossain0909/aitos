60cb35864536dc66c75e6018ee2482028cdbb334
Fahad Hossain <md.fahad.hossain.0909@gmail.com>
2026-08-29 10:18:11 +0600

    ci: fix isort ordering in models init

--- aitos/models/__init__.py
+++ aitos/models/__init__.py
@@ -1,5 +1,4 @@
-from .market import FundingRate, Kline, OpenInterest, OrderBookSnapshot, TradeTick
-from .market import TradeSide as MarketTradeSide
+from .market import FundingRate, Kline, OpenInterest, OrderBookSnapshot, TradeSide as MarketTradeSide, TradeTick
 from .trade import Opportunity, PartialExit, Trade, TradeLifecycleState, TradeSide
 
 __all__ = [
