a397c229f46a7d7388b51f48283bebc0577604e0
Fahad Hossain <md.fahad.hossain.0909@gmail.com>
2026-09-01 12:59:32 +0600

    feat: wire trade transport telemetry into ingestion

--- aitos/data/__init__.py
+++ aitos/data/__init__.py
@@ -1,5 +1,8 @@
 from .ingestion import DataIngestionService, kline_topic, orderbook_topic, trade_topic
 from .repository import MarketDataRepository
+from .transport_telemetry import install_transport_telemetry
+
+install_transport_telemetry(DataIngestionService)
 
 __all__ = [
     "DataIngestionService",
