3ffa5b2a5c3e05eeda75429a24974b594bc74292
Fahad Hossain <md.fahad.hossain.0909@gmail.com>
2026-09-01 21:57:49 +0600

    fix: install stale REST trade boundary guard

--- aitos/exchange/__init__.py
+++ aitos/exchange/__init__.py
@@ -1,7 +1,10 @@
 from .base import ExchangeAdapter
 from .binance import BinanceFuturesAdapter
 from .rate_limiter import TokenBucketRateLimiter
+from .rest_trade_guard import install_rest_trade_guard
 from .symbol_filters import SymbolFilters, parse_exchange_info
+
+install_rest_trade_guard(BinanceFuturesAdapter)
 
 __all__ = [
     "BinanceFuturesAdapter",
