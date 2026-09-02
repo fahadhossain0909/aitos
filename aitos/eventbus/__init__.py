7cd86defe110427cb04ac973b410d9f6fe309e0e
Fahad Hossain <md.fahad.hossain.0909@gmail.com>
2026-09-03 00:01:47 +0600

    forensic: install end-to-end market data telemetry

--- aitos/eventbus/__init__.py
+++ aitos/eventbus/__init__.py
@@ -1,7 +1,9 @@
 from aitos.forensics.market_data_attribution import install_eventbus_attribution
+from aitos.forensics.end_to_end_telemetry import install as install_end_to_end_telemetry
 
 from .redis_bus import DLQ_STREAM, EventBus, Subscription, validate_event_schema
 
 install_eventbus_attribution(EventBus)
+install_end_to_end_telemetry(EventBus)
 
 __all__ = ["DLQ_STREAM", "EventBus", "Subscription", "validate_event_schema"]
