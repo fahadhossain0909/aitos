28c2203534fefdf16ca008425d2b4a20aae44312
Fahad Hossain <md.fahad.hossain.0909@gmail.com>
2026-09-02 10:01:25 +0600

    forensic: attribute market-data consumer latency

--- aitos/eventbus/__init__.py
+++ aitos/eventbus/__init__.py
@@ -1,3 +1,6 @@
 from .redis_bus import DLQ_STREAM, EventBus, Subscription, validate_event_schema
+from aitos.forensics.market_data_attribution import install_eventbus_attribution
+
+install_eventbus_attribution(EventBus)
 
 __all__ = ["DLQ_STREAM", "EventBus", "Subscription", "validate_event_schema"]
