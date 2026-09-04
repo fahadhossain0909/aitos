69774c4b0d7ccefd3f5cda57748f55106579e22f
Fahad Hossain <md.fahad.hossain.0909@gmail.com>
2026-09-05 00:51:10 +0600

    chore(debug): inspect lifecycle capital gate

---
+++ tests/backtest/test_tmp_lifecycle_capital_diagnostic.py
@@ -0,0 +1,10 @@
+import dis
+
+from aitos.trading.lifecycle import TradeLifecycle
+
+
+def test_tmp_lifecycle_capital_diagnostic():
+    fn = TradeLifecycle.submit_opportunity
+    print("SUBMIT_NAMES", fn.__code__.co_names)
+    print("SUBMIT_CONSTS", fn.__code__.co_consts)
+    dis.dis(fn)
