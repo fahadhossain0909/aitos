b114f8e81c663c0cc73f16bcab2e27711261ac60
Fahad Hossain <md.fahad.hossain.0909@gmail.com>
2026-09-04 21:23:25 +0600

    feat: activate capital objective runtime guard

--- aitos/intelligence/__init__.py
+++ aitos/intelligence/__init__.py
@@ -8,6 +8,7 @@ from .capital_objective import (
     CapitalObjectiveConfig,
     OpportunityEstimate,
 )
+from .capital_runtime import install_capital_guard
 from .deep_rl_policy import DeepValueRLScorer
 from .funding import funding_rate_score
 from .liquidity import liquidity_quality_score
@@ -43,6 +44,7 @@ __all__ = [
     "determine_direction",
     "funding_rate_score",
     "indicators",
+    "install_capital_guard",
     "liquidity_quality_score",
     "oi_trend_score",
 ]
